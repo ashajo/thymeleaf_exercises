a59a835a2ef3fdece22d2d79fb150b48a313bcaf
asha joseph <ashajo183@gmail.com>
2021-04-18 21:13:11 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/thymeleaf_exercises/ThymeleafExercisesApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.thymeleaf_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/thymeleaf_exercises/ThymeleafExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.thymeleaf_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
